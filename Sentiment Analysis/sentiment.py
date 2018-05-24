9f7bb331a84fde9e266670dc875713e247857038
Jatin <jatinj615@gmail.com>
2018-05-24 18:04:59 +0530

    optimization

--- Sentiment Analysis/sentiment.py
+++ Sentiment Analysis/sentiment.py
@@ -3,7 +3,7 @@ from tflearn.data_utils import to_categorical, pad_sequences
 from tflearn.datasets import imdb
 
 ## Loading Dataset
-train, test, _ = imdb.load_data(path='imdb.pkl', n_words = 10000, valid_portion = 0.1)
+train, test, _ = imdb.load_data(path='imdb.pkl', n_words = 10000, valid_portion = 0.2)
 trainX, trainY = train
 testX, testY = test
 
@@ -20,8 +20,8 @@ net = tflearn.input_data([None, 100])
 net = tflearn.embedding(net, input_dim = 10000, output_dim = 128)
 net = tflearn.lstm(net, 128, dropout = 0.8)
 net = tflearn.fully_connected(net, 2, activation = 'softmax')
-net = tflearn.regression(net, optimizer = 'adam', learning_rate = 0.0001, loss = 'categorical_crossentropy')
+net = tflearn.regression(net, optimizer ='adam', learning_rate = 0.0001, loss = 'categorical_crossentropy')
 
 # Training model
 model = tflearn.DNN(net, tensorboard_verbose = 0)
-model.fit(trainX, trainY, validation_set = (testX, testY), show_metric = True, batch_size = 32)
+model.fit(trainX, trainY, validation_set = (testX, testY), show_metric = True,batch_size = 32)
